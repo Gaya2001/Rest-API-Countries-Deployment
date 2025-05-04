5cb51fc93d97ec417941c8cb57801b0b5e07a5fe
kavindu gayashan <127669394+Gaya2001@users.noreply.github.com>
2025-05-04 22:11:13 +0530

    allow permission for the render

--- Front-End/vite.config.js
+++ Front-End/vite.config.js
@@ -6,8 +6,15 @@ export default defineConfig({
   plugins: [react()],
 
   server: {
-    host: '0.0.0.0',
-    port: 5173
-  }
+    host: '0.0.0.0',  // Make the server accessible on all network interfaces
+    port: 5173,
+    allowedHosts: [
+      'rest-api-countries-deployment-1.onrender.com',  // Allow this host
+      'localhost', // Add localhost if needed for local development
+      '127.0.0.1',  // Add localhost IP
+      '44.226.145.213',
+      '54.187.200.255'
 
+    ]
+  }
 })
